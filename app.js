4dcd7efa01dd4247270317632e3ac92400609eee
michielmulders <michielmulders1@gmail.com>
2020-04-18 23:23:42 +0200

    Add body parser for JSON

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const config = require('./config')
+// https://www.npmjs.com/package/http-errors
 
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (_, res) => {
   res.send('API up and running')
